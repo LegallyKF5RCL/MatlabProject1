f0c391e027068e7a0e8f6e06947fc3f6ea33ebd5
LegallyKF5RCL <kf5rcl@gmail.com>
2013-09-29 20:39:08 -0500

    First Commit

---
+++ Project1/CosWavePlot.m
@@ -0,0 +1,54 @@
+clc;
+clear all;
+
+Fs = 1000;
+
+A = 5;
+f = 50;
+phase = 0;
+
+t = 0:1/Fs:1;
+
+x=A*cos(2*pi*f*t + phase);
+
+plot(t,x);
+figure;
+
+FT_x = fft(x);
+
+correct_FT_x = fftshift(FT_x);
+
+plot(FT_x)
+figure;
+%FT_x(2)        %this is indexing the array
+
+mag = abs(FT_x);
+ph = angle(FT_x);
+
+%QWER = linspace(f-Fs/2: : Fs/2);
+
+plot(f,mag);
+figure;
+plot(f,ph);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
